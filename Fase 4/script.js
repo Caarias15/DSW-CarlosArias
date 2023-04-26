41a9d3e2a425047179a3c5b73d0cf7de3b07be06
Caarias15 <caariasor@unadvirtual.edu.co>
2023-04-26 15:47:31 -0500

    FASE4

---
+++ Fase 4/script.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+//EJEMPLOS EN CLASE 4
+// let miArreglo = [6,7,3,1]
+// document.getElementById('salida').innerHTML="este es el elmento de salida"
+
+//EJEMPLO 3
+//for(let i=0; i< miArreglo.length; i++ ){
+//document.write(miArreglo[i]); }
+
+// EJEMPLO 2
+//console.log('el valor es '+miArreglo[3]);
+
+//EJEMPLO 1
+//let x = 4;
+//let y = 3;
+
+// if (x === y) {
+// console.log('los numeros son iguales');
+// }
+// else {
+//     console.log( 'no son iguales')
+// }
+ 
+
