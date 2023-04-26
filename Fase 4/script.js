53bb8387a77d34cb8a83fdc0e5b3ba64a6ed266d
Caarias15 <caariasor@unadvirtual.edu.co>
2023-04-26 16:52:40 -0500

    fase4

--- Fase 4/script.js
+++ Fase 4/script.js
@@ -1,5 +1,25 @@
 
+function procesarRespuestas(){
+    let total = 5
+    let puntos = 0;
 
+    let myForm =document.forms["formulario"];
+    let respuestasCorrectas =["c", "a", "a", "b", "d",]
+
+    for (let i=1; i<= total; i++){
+        if (myForm["r"+i].value == null ||
+            myForm["r"+i].value == '') {
+            alert('Favor responder todas las preguntas del cuestionario');
+            return false;    
+        } else {
+            if (myForm["r"+i].value === respuestasCorrectas[i -1])
+                puntos++;
+        }
+    }
+    let resultado = document.getElementById('resultado')
+    resultado.innerHTML='Obtuviste '+puntos+' puntos de ' + total + ' posibles ';
+    return false;
+}
 
 
 
